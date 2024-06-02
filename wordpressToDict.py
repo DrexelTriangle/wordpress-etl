9c344165091edd2aabbe0c7e86f9c3af4ebb4004
nerdhotline <kenny.g.blake@gmail.com>
2024-06-01 22:50:34 -0400

    Cleaned [wordpressToDict.py], added file location variables for Kenneth and Cole

--- wordpressToDict.py
+++ wordpressToDict.py
@@ -14,11 +14,20 @@ import re
 
 
 print("imports -> successful")
+
+kenneth_wpFile = "C:\\Users\\kenne\\OneDrive\\Documents\\GitHub\\shkenanigans\\tri-wpdump_4-1-24.xml"
+kenneth_file2_loc = "C:\\Users\\kenne\OneDrive\\Documents\\GitHub\\shkenanigans\\test_dump.txt"
+kenneth_file3_loc = "C:\\Users\\kenne\\OneDrive\\Documents\\GitHub\\shkenanigans\\articleDump.txt"
+
+cole_wpFile = "C:\\Users\\Cole\\skennanigans\\shkenanigans\\tri-wpdump_4-1-24.xml"
+cole_file2_loc = "C:\\Users\\Cole\\skennanigans\\shkenanigans\\test_dump.txt"
+cole_file3_loc = "C:\\Users\\Cole\\skennanigans\\shkenanigans\\articleDump.txt"
+
+
 wp_xmlContent = None
-wpFile = "C:\\Users\\Cole\\skennanigans\\shkenanigans\\tri-wpdump_4-1-24.xml"
-# file1_loc = "C:\\Users\\kenne\\OneDrive\Desktop\\The Sandbox\\PythonSandbox\\wpDict.json"
-file2_loc = "C:\\Users\\Cole\\skennanigans\\shkenanigans\\test_dump.txt"
-file3_loc = "C:\\Users\\Cole\\skennanigans\\shkenanigans\\articleDump.txt"
+wpFile = kenneth_wpFile
+file2_loc = kenneth_file2_loc
+file3_loc = kenneth_file3_loc
 
 
 # Grab XML Content  
@@ -31,59 +40,11 @@ print("Obtained File Contents!")
 # convert xml to dictionary  
 print("Converting XML to Dictionary")
 myDict = xmltodict.parse(wp_xmlContent)
-
 print("Already Loaded")
 
 # Grab Author Data
 authorData = myDict.get('rss').get('channel').get('wp:author')
 articleData = myDict.get('rss').get('channel').get('item')
-
-    
-
-
-# # Generate author list.
-# pb.printProgressBar(0, len(authorData), length = 20)
-# for i, item in enumerate(authorData):
-#     fName = ''
-#     lName = ''
-#     if ( (authorData[i].get('wp:author_first_name') is None) and (authorData[i].get('wp:author_last_name') is None)):
-#         name = util.charMorph(authorData[i].get ('wp:author_display_name'))
-#         newName = util.parseName(name)
-#         fName = newName[0]
-#         lName = newName[1]
-#     else:
-#         fName = util.charMorph(authorData[i].get('wp:author_first_name'))
-#         lName = util.charMorph(authorData[i].get('wp:author_last_name'))
-#         email = util.charMorph(authorData[i].get('wp:author_email'))
-#         obj = c.wpAuthor(fName, lName, email)
-#     time.sleep(0.00001)
-
-#     pb.printProgressBar(i, len(authorData), length = 20)
-# print('Author List Generated')
-
-# # c.wpAuthor.printAuthors()
-# # with open(file3_loc, "w") as file:
-# #     file.write(c.wpAuthor.authorTemp)
-# #     file.close()
-
-
-
-# print("\n")
-
-
-
-
-
-
-# print(articleData[268].get('dc:creator'))
-# print(articleData[268].get('title')) # title = title
-# print(util.GMT_to_EST(articleData[268].get('wp:post_date_gmt'))) # pubDate = pubDate
-# print(util.GMT_to_EST(articleData[268].get('wp:post_modified_gmt'))) # modDate = modDate 
-# print(articleData[268].get('description')) # description = description
-# print(articleData[268].get('wp:comment_status')) # commentStatus = commentStatus
-# print(f'''{util.processArticleTags(articleData[268].get('category'))}''')
-# print(articleData[268].get('content:encoded')) # text = text
-
 
 # Generate article name list.
 pb.printProgressBar(0, len(articleData), length = 20)
@@ -116,30 +77,6 @@ for i, item in enumerate(articleData):
     pb.printProgressBar(i, len(articleData), length = 20)
 # print(c.wpArticle.generic_articleDict[1])
 print('Article List Generated')
-c.wpArticle.printArticles(file3_loc)
-
-
-
-
-
-
-
-# for i, item in enumerate(articleData):
-    # id = id
-    # title = title
-    # pubDate = pubDate
-    # modDate = modDate 
-    # description = description
-    # commentStatus = commentStatus
-    # tags = tags
-    # text = text
-
-
-
-
-
-# TODO: merge in guest authors......
- 
-
-
-
+with open(file3_loc, "w") as file:
+    file.write(c.wpArticle.printArticles())
+    file.close()

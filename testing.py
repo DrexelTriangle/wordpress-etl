c3c9f72e6515f0cf7d7f9d4147744d1bc718cd8f
nerdhotline <kenny.g.blake@gmail.com>
2024-06-25 08:30:00 -0400

    added more testing features

--- testing.py
+++ testing.py
@@ -1,6 +1,13 @@
 from classes.wpArticle import *
+from pprint import *
 
 def writeArticlesToFile(fileLoc):
     with open(fileLoc, "w") as file:
         wpArticle.printArticles(fileLoc)
+        file.close()
+
+def visualizeDictionary(myDict, fileLoc):
+    output = pprint.pformat(myDict)
+    with open(fileLoc, 'w+') as file:
+        file.write(output)
         file.close()

98d3524c8d27432f02f1549685fb089dedda5561
nerdhotline <79532110+nerdhotline@users.noreply.github.com>
2024-11-04 07:55:15 -0500

    Cleaned Author.visualize()

--- Author.py
+++ Author.py
@@ -43,59 +43,14 @@ class Author:
   
   def visualize():
     # visualized authors
-
-    noFirst, noLast, noEmail, normal = 0, 0, 0, 0
-    isNoFirst, isNoLast, isNoEmail, isNormal = False, False, False, False
-
     with open('.\\visualizations\\visualized-authors.txt', 'w+', encoding='utf-8') as file:
       result = ''
       for i in range(len(Author.authorDict)):
         file.write(str(Author.getAuthor(i)))
       file.close()
     
-    with open('.\\stats\\authors-no-first-name.txt', 'w+', encoding='utf-8') as file_1, \
-      open('.\\stats\\authors-no-last-name.txt', 'w+', encoding='utf-8') as file_2, \
-      open('.\\stats\\authors-no-email.txt', 'w+', encoding='utf-8') as file_3, \
-      open('.\\stats\\authors-normal.txt', 'w+', encoding='utf-8') as file_4:
-      result = ''
-      for i in range(len(Author.authorDict)):
-        author = Author.getAuthor(i)
-        isNoFirst = author.firstName == 'NO_FIRST'
-        isNoLast = author.lastName == 'NO_LAST'
-        isNoEmail = author.email == 'NO_EMAIL'
-        isNormal = not(isNoFirst or isNoLast or isNoEmail)
-        
-        if isNoFirst:
-          file_1.write(str(Author.getAuthor(i)))
-          noFirst += 1
-
-        if isNoLast:
-          file_2.write(str(Author.getAuthor(i)))
-          noLast += 1
-
-        if isNoEmail:
-          file_3.write(str(Author.getAuthor(i)))
-          noEmail += 1
-        
-        if isNormal:
-          file_4.write(str(Author.getAuthor(i)))
-          normal += 1
-            
-
-      file_1.close()
-      file_2.close()
-      file_3.close()
-      file_4.close()
-      # print(f'> [author.process-authors] stats:')
-      # if noFirst > 0:
-      #    print(f'\t\t\t  {noFirst} with no first name')
-      # if noLast > 0:
-      #    print(f'\t\t\t  {noLast} with no last name')
-      # if noEmail > 0:
-      #    print(f'\t\t\t  {noEmail} with no email')
-      # if normal > 0:
-      #    print(f'\t\t\t  {normal} normal')
-      # print(f'\t\t\t  {len(Author.authorDict)} total')
+    
+      
 
   def processAuthors(authorData):
     print('> [author.process-authors] processing authors...')

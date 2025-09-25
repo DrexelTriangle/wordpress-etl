1d380dc7616a27e7cbcea9059e431067ef7176cb
nerdhotline <79532110+nerdhotline@users.noreply.github.com>
2025-09-25 01:24:51 -0400

    Added visualize dictionary

--- Utility.py
+++ Utility.py
@@ -1,11 +1,19 @@
 import zipfile
 import shutil
+import pprint
 
 class Utility:
   def unzip(zipPath):
     with zipfile.ZipFile(zipPath, 'r') as zip_ref:
       zip_ref.extractall('.\\Data')
 
-      
+
   def _delete_dir(dir):
     shutil.rmtree(dir)
+
+  
+  def _visualize(dict, fileName):
+    result = pprint.pformat(dict)
+    with open(fileName, 'w+', encoding='utf-8') as file:
+      file.write(result)
+      file.close()

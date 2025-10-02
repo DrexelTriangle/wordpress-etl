67561c47067ef73724e304f46618f47219ee1f33
nerdhotline <79532110+nerdhotline@users.noreply.github.com>
2025-10-02 19:04:33 -0400

    Cmments

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from Extractor import Extractor
 from Utility import *
 from Constants import *
 
+# TODO: python library dependency checks
+
+# unzip export data
 Utility.unzip(ZIP_FILE)
+
+# STEP 1: Extraction
 extractor = Extractor(POSTS_FILE, GUEST_AUTH_FILE)
 result = extractor.getData()
+

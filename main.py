5fa706b4aaee1eb1c05614552b69af278a494b33
Han <baohanmi@gmail.com>
2025-10-21 14:53:58 -0400

    Added all Translator to main

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from Translator.Translator import *
 from Utility import *
 from Constants import *
 from Translator.ArticleTranslator import *
+from Translator.GuestAuthorTranslator import *
+from Translator.AuthorTranslator import *
 
 # TODO: python library dependency checks
 
@@ -12,24 +14,33 @@ Utility.unzip(ZIP_FILE)
 # STEP 1: Extraction
 extractor = Extractor(POSTS_FILE, GUEST_AUTH_FILE)
 extracted = extractor.getData() 
-
+'''
+FORMAT OF EXTRACTED DATA
+{
+  auth: ...
+  guestAuth: ...
+  art: ...
+}
+'''
 translators = {
-  "articles": ArticleTranslator(extracted["art"])
+  "articles": ArticleTranslator(extracted["art"]),
+  "gAuth": GuestAuthorTranslator(extracted["guestAuth"]),
+  "auth": AuthorTranslator(extracted["auth"])
 }
 
-translators["articles"].translate()
-translators["articles"]._visualize()
-names = sorted(list(translators["articles"].uniqueAuthorCleanNames))
-longestName = len(max(names, key=len))
+translators["gAuthor"].translate()
+translators["gAuthor"]._log()
+# names = sorted(list(translators["articles"].uniqueAuthorCleanNames))
+# longestName = len(max(names, key=len))
 
 
-for i in range(len(names)):
-  authorName = names[i]
-  spaces = longestName - len(authorName)
-  buf = (' ' * spaces) + authorName
-  print(buf, end=' ')
-  if (i + 1) % 4 == 0:
-    print()
+# for i in range(len(names)):
+#   authorName = names[i]
+#   spaces = longestName - len(authorName)
+#   buf = (' ' * spaces) + authorName
+#   print(buf, end=' ')
+#   if (i + 1) % 4 == 0:
+#     print()
 
 
 

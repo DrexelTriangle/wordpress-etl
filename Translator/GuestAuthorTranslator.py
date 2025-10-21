cfc168e9db0b6a48b02785f429e2f578359b7d53
nerdhotline <79532110+nerdhotline@users.noreply.github.com>
2025-10-21 18:26:50 -0400

    GuestAuthorTranslator: removed _log(), and moved metadata processing into independent function

--- Translator/GuestAuthorTranslator.py
+++ Translator/GuestAuthorTranslator.py
@@ -8,27 +8,26 @@ class GuestAuthorTranslator(Translator):
     def __init__(self, source):
         super().__init__(source)
     
+    def _extractMetadata(self, data):
+        displayName, firstName, lastName, email, login = '', '', '', '', ''
+        for metadata in data:
+            match metadata['wp:meta_key']:
+                case 'cap-display_name':
+                    displayName = metadata['wp:meta_value']
+                case 'cap-user_email':
+                    email = metadata['wp:meta_value']
+                case 'cap-first_name':
+                    firstName = metadata['wp:meta_value']
+                case 'cap-last_name':
+                    lastName = metadata['wp:meta_value']
+                case 'cap-user_login':
+                    login = metadata['wp:meta_value']
+        return [displayName, firstName, lastName, email, login, self.objCount]
+
+
     def translate(self):
         for guestAuthorData in self.source:
-            # Find data in raw data
-            for metadata in guestAuthorData['wp:postmeta']:
-                match metadata['wp:meta_key']:
-                    case 'cap-display_name':
-                      displayName = metadata['wp:meta_value']
-                    case 'cap-user_email':
-                        email = metadata['wp:meta_value']
-                    case 'cap-first_name':
-                        firstName = metadata['wp:meta_value']
-                    case 'cap-last_name':
-                        lastName = metadata['wp:meta_value']
-                    case 'cap-user_login':
-                        login = metadata['wp:meta_value']
-            
-            guestAuthorObject = GuestAuthor(displayName, firstName, lastName, email, login, self.objCount)
+            metadata = self._extractMetadata(guestAuthorData['wp:postmeta'])
+            guestAuthorObject = GuestAuthor(*metadata)
             self.objDataDict.update({guestAuthorObject.data["id"]: guestAuthorObject.data})
-            self.objCount += 1
-
-    def _log(self):
-        with open('log\\gAuth.json', 'w+', encoding="utf-8") as file:
-            json.dump(self.objDataDict, file, indent=4)
-            
+            self.objCount += 1 

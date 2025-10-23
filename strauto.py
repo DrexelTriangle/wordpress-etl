4fd08d3ec1eb2576ced4c1264dcce5913cc82b02
nerdhotline <79532110+nerdhotline@users.noreply.github.com>
2025-10-23 01:08:10 -0400

    Deleted test files

--- strauto.py
+++
@@ -1,8 +0,0 @@
-ga = {
-                "display_name": "",
-                "first_name": "first_name",
-                "last_name": "last_name",
-        } 
-
-for key in ga:
-    print("result += f'\\t%s: {self.data[\"%s\"]}\\n'" % (key, key))

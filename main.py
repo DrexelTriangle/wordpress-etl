d1592ec0340139eef95174532f8ce7da7be19f06
nerdhotline <79532110+nerdhotline@users.noreply.github.com>
2025-10-23 11:25:10 -0400

    Article logger splits json file into 1000-long json files

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ for key in translators:
 
 
 # DEBUG: logging
-translators["articles"]._log('log\\articles.json')
+translators["articles"]._log('log\\articles')
 translators["gAuth"]._log('log\\gAuth.json')
 translators["auth"]._log('log\\auth.json')
 

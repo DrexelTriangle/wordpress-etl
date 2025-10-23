a6564fbb7ed5c1d63c38eb54d3d5fb8ae0bb2844
nerdhotline <79532110+nerdhotline@users.noreply.github.com>
2025-10-22 21:37:53 -0400

    Made ArticleTranslator/Article classes parallel behavior from gAuth and Author classes

--- Translator/ArticleTranslator.py
+++ Translator/ArticleTranslator.py
@@ -1,7 +1,7 @@
 from Translator.Translator import Translator
 from Translator.Article import Article
 import os as OS
-import json
+from Utility import Utility as U
 
 class ArticleTranslator(Translator):  
   # Constructor
@@ -9,23 +9,48 @@ class ArticleTranslator(Translator):
     super().__init__(incomingData)
     self.uniqueAuthorCleanNames = set()
 
-    
-  def translate(self):
-    for i, item in enumerate(self.source):
-      obj = Article()
-      obj.setData(self.objCount, self.source[i])
-      title = obj["title"]
+  def _getArticleData(self, data):
+    authorIDs = []
+    authors = []
+    authorCleanNames = []
+    breakingNews = False 
+    commentStatus = data.get('wp:comment_status', Article.defaultValue)
+    description = U._html_text_norm(data.get('description'))
+    featuredImgID = -1
+    id = self.objCount
+    priority = False 
+    modDate = data.get('wp:post_modified_gmt', Article.defaultValue),
+    photoCred = None
+    pubDate = data.get('wp:post_date_gmt', Article.defaultValue)
+    tags = data.get('category')
+    text = str(U._html_text_norm(data.get('content:encoded', Article.defaultValue))).replace('"', '\\"')
+    title = U._html_text_norm(data.get('title', Article.defaultValue))
 
+    return [
+      authorIDs, authors, authorCleanNames,
+      breakingNews, commentStatus, description,
+      featuredImgID, id, priority, modDate, 
+      photoCred, pubDate, tags, text, title
+    ]
+
+
+
+  def translate(self):
+    for i, itm in enumerate(self.source):
+      objData = self._getArticleData(itm)
+      obj = Article(*objData)
+      obj.processTags()
+      
+      # NOTE: using to only load 9k of the article data
+      title = obj["title"]
       if (title is not None):
         obj["title"] = title.replace('"', '\\"')
-      
-      obj.processTags()
       if (obj["title"] is None or not obj.dataSanityCheck() or obj["tags"] == -1):
         continue
     
       self.objDataDict.update({obj["id"]: obj.data})
-      self.uniqueAuthorCleanNames.update(obj["authorCleanNames"])
-      self.objCount += 1
+      self.addObject(obj)
+
 
   def _printUniqueAuthors(self):
     terminal_size = OS.get_terminal_size()

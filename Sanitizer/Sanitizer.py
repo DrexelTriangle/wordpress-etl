f158db1017cd9f362699c7f702d041c61df6813a
ssavutu <ssavutu@gmail.com>
2025-11-10 11:12:45 -0500

    Created Sanitizer Parent Class

---
+++ Sanitizer/Sanitizer.py
@@ -0,0 +1,12 @@
+class Sanitizer:
+    def __init__(self, source):
+        self.source = source
+
+    def clean(self):
+        pass
+
+    def autoResolve(self):
+        pass
+
+    def manualResolve(self):
+        pass

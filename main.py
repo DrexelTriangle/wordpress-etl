ae8c31572b34c84b2450a0ca176ea7afa7d42f4c
nerdhotline <79532110+nerdhotline@users.noreply.github.com>
2025-12-09 17:50:53 -0500

    main: Implemented Formatters

--- main.py
+++ main.py
@@ -24,6 +24,25 @@ def extractData():
   Utility.unzip(ZIP_FILE)
   extractor = Extractor(*UNZIPPED_FILES)
   return runStep("Extracting...", "Extracted", extractor.getData)
+from Translator.Translator import *
+from Utils.Utility import *
+from Utils.Constants import *
+from Translator.ArticleTranslator import *
+from Translator.GuestAuthorTranslator import *
+from Translator.AuthorTranslator import *
+from Formatter.ArticleFormatter import *
+from Formatter.AuthorFormatter import *
+from Formatter.GuestAuthorFormatter import *
+from Sanitizer.AuthorSanitizer import *
+
+# TODO: python library dependency checks
+
+# unzip export data
+Utility.unzip(ZIP_FILE)
+
+# STEP 1: Extraction
+extractor = Extractor(*UNZIPPED_FILES)
+extracted = extractor.getData() 
 
 def translateData(extracted):
   translators = {
@@ -81,3 +100,40 @@ writeAuthorOutput(authors, "logs/auth_output.json", "author")
 guestAuthors = sanitizeAuthors(translators, "gAuth", "guest authors")
 writeAuthorOutput(guestAuthors, "logs/gauth_output.json", "guest author")
 printChecklist()
+
+# Step 2: Translation
+translators = {
+  "articles": ArticleTranslator(extracted["art"]),
+  "gAuth": GuestAuthorTranslator(extracted["guestAuth"]),
+  "auth": AuthorTranslator(extracted["auth"])
+}
+
+for key in translators:
+  translators[key].translate()
+
+
+# # DEBUG: logging
+# translators["articles"]._log('log\\articles')
+# translators["gAuth"]._log('log\\gAuth.json')
+# translators["auth"]._log('log\\auth.json')
+
+
+# Step 3: Sanitation
+authorSanitizer = AuthorSanitizer(translators["auth"].getObjList(), {})
+authorSanitizer.sanitize()
+
+
+# Step 4: Formatting
+formatters = {
+  "articles": ArticleFormatter(translators["articles"]),
+  "gAuth": GuestAuthorFormatter(translators["auth"]),
+  "auth": AuthorFormatter(translators["gAuth"]),
+}
+
+for key in formatters:
+  formatters[key].format()
+
+
+
+
+

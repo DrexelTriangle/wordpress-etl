cabb36b9080f2688de2644ae0ea51dedba972f1a
nt636@tux.cci.drexel.edu <khanhnguyentrinhthi@gmail.com>
2026-02-11 20:32:36 -0500

    Delete guest author list after merging

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ app = App()
 
 # MASS IMPORT/EXPORT
 def build():
+    # Extract and translate
     extracted = app.extractData()
     translators = app.translateData(extracted)
     app.logOutputs(translators)
+    
+    # Sanitize authors
     authors = app.sanitizeAuthors(translators, "auth", "authors")
     app.writeAuthorOutput(authors, "logs/auth_output.json", "author")
     guestAuthors = app.sanitizeAuthors(translators, "gAuth", "guest authors")
     app.writeAuthorOutput(guestAuthors, "logs/gauth_output.json", "guest author")
     
-    # Combine and reindex all authors
+    # Combine auth
     allAuthors = app.combineAndReindexAuthors(authors, guestAuthors)
+    del guestAuthors
     app.writeAuthorOutput(allAuthors, "logs/merged_auth_output.json", "merged authors")
     
+    # Sanitize article
     sanitizedArticles = app.sanitizeArticleAuthors(translators, allAuthors)
     sanitizedArticles = app.sanitizeArticleContent(sanitizedArticles)
     app.writeArticleOutput(sanitizedArticles)

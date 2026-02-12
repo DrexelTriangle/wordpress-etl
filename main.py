5a061adf5e160b73a7a0597055bd5801be303df9
nerdhotline <kenny.g.blake@gmail.com>
2026-02-11 20:40:07 -0500

    Formatting + Merged Sanitized author lists

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ app = App()
 
 # MASS IMPORT/EXPORT
 def build():
+    # EXTRACTION
     extracted = app.extractData()
+
+    # TRANSLATION
     translators = app.translateData(extracted)
     app.logOutputs(translators)
+    
+    # SANITATION
     authors = app.sanitizeAuthors(translators, "auth", "authors")
-    app.writeAuthorOutput(authors, "logs/auth_output.json", "author")
     guestAuthors = app.sanitizeAuthors(translators, "gAuth", "guest authors")
+    app.writeAuthorOutput(authors, "logs/auth_output.json", "author")
     app.writeAuthorOutput(guestAuthors, "logs/gauth_output.json", "guest author")
+    authors = app.mergeAuthLists(authors, guestAuthors)
     sanitizedArticles = app.sanitizeArticleAuthors(translators, authors, guestAuthors)
     sanitizedArticles = app.sanitizeArticleContent(sanitizedArticles)
     app.writeArticleOutput(sanitizedArticles)
+    
     
     app.printChecklist()
 

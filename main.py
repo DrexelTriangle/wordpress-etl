4afb23ea0d4e73cd4106e8a813f98994c796b8e8
ssavutu <ssavutu@gmail.com>
2026-02-13 06:09:51 -0500

    Add graceful termination (this is your fault Kenneth)

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 from App import App
+import traceback
 
 app = App()
 
-# MASS IMPORT/EXPORT
 def build():
-    # Extract and translate
-    extracted = app.extractData()
+    try:
+        extracted = app.extractData()
 
-    # TRANSLATION
-    translators = app.translateData(extracted)
-    app.logOutputs(translators)
-    
-    # Sanitize authors
-    authors = app.sanitizeAuthors(translators, "auth", "authors")
-    guestAuthors = app.sanitizeAuthors(translators, "gAuth", "guest authors")
-    app.writeAuthorOutput(authors, "logs/auth_output.json", "author")
-    app.writeAuthorOutput(guestAuthors, "logs/gauth_output.json", "guest author")
-    
-    # Combine auth
-    allAuthors = app.combineAndReindexAuthors(authors, guestAuthors)
-    del guestAuthors
-    app.writeAuthorOutput(allAuthors, "logs/merged_auth_output.json", "merged authors")
-    
-    # Sanitize article
-    sanitizedArticles = app.sanitizeArticleAuthors(translators, allAuthors)
-    sanitizedArticles = app.sanitizeArticleContent(sanitizedArticles)
-    app.writeArticleOutput(sanitizedArticles)
-    
-    
-    app.printChecklist()
+        translators = app.translateData(extracted)
+        app.logOutputs(translators)
+        
+        authors = app.sanitizeAuthors(translators, "auth", "authors")
+        guestAuthors = app.sanitizeAuthors(translators, "gAuth", "guest authors")
+        app.writeAuthorOutput(authors, "logs/auth_output.json", "author")
+        app.writeAuthorOutput(guestAuthors, "logs/gauth_output.json", "guest author")
+        
+        allAuthors = app.combineAndReindexAuthors(authors, guestAuthors)
+        del guestAuthors
+        app.writeAuthorOutput(allAuthors, "logs/merged_auth_output.json", "merged authors")
+        
+        sanitizedArticles = app.sanitizeArticleAuthors(translators, allAuthors)
+        sanitizedArticles = app.sanitizeArticleContent(sanitizedArticles)
+        app.writeArticleOutput(sanitizedArticles)
+        
+        app.printChecklist()
+    except Exception:
+        app.shutdown()
+        traceback.print_exc()
 
 build()

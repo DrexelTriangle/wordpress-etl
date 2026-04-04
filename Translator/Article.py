03078a0c8de4bc5e42b771d0c4a1813808aba87b
ssavutu <ssavutu@gmail.com>
2026-04-04 00:33:13 -0400

    Misc

--- Translator/Article.py
+++ Translator/Article.py
@@ -29,7 +29,7 @@ class Article(WPO):
       "categories": [],
       "metadata": metadata,
       "text": text,
-      "title": title, 
+      "title": title,
     }
   
   
--- Translator/ArticleTranslator.py
+++ Translator/ArticleTranslator.py
@@ -118,4 +118,3 @@ class ArticleTranslator(Translator):
 
 
   
-
